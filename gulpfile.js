2ec75cb29f4d748dda2a084066cf0b019bc2811f
The U <luis.salazar@wpengine.com>
2016-11-05 12:13:43 -0500

    dist and public/dist

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+
+gulp.task('default', function() {
+
+// Node source
+ gulp.src("es6/**/*.js")
+        .pipe(babel({presets: ["es2015"]}))
+.pipe(gulp.dest("dist"));
+
+ // browser source
+ gulp.src("public/es6/**/*.js")
+        .pipe(babel({presets: ["es2015"]}))
+        .pipe(gulp.dest("public/dist"));
+    });
---
+++ public/es6/test.js
@@ -0,0 +1,11 @@
+  // es6 feature: block-test public es6 "let" declaration
+let sentences = [
+{ subject: 'JavaScript', verb: 'is', object: 'great' }, { subject: 'Elephants', verb: 'are', object: 'large' },
+];
+    // es6 feature: object destructuring
+function say({ subject, verb, object }) {
+// es6 feature: template strings console.log(`${subject} ${verb} ${object}`);
+}
+    // es6 feature: for..of
+for(let s of sentences) { say(s);
+}
